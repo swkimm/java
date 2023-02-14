b75582b5271a274e547f5c52e46dc32ea0bb4071
swkimm <swkim7161@kakao.com>
2023-02-14 10:54:35 +0900

    first commit

---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		System.out.println("자바자바자");
+		System.out.println("출력 되는 내용");
+
+		// 한줄 지우기 ctrl + d
+		// 줄 옮기기 option + 위 아래 
+		// 줄 복사 option + command + 위 아래 
+		
+	}
+}
---
+++ src/ch01/sec11/ManMethod.java
@@ -0,0 +1,8 @@
+package ch01.sec11;
+
+public class ManMethod {
+
+	public static void main(String[] args) {
+		System.out.println("메인 메소드 실행");
+	}
+}

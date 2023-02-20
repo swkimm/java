da243c8c493d759ca94a6d55ddadb11e02e1363c
swkimm <swkim7161@kakao.com>
2023-02-20 09:11:58 +0900

    Create C01Conditional.java

---
+++ src/ch03/lecture/p05conditional/C01Conditional.java
@@ -0,0 +1,19 @@
+package ch03.lecture.p05conditional;
+
+public class C01Conditional {
+	public static void main(String[] args) {
+		// 삼항(ternary), 조건(conditional), ?
+		// 1항 ? 2항 : 3항
+		// 연산 결과 : 1항이 true면 2항, false면 3항
+		
+		int x = 5;
+		int y = 10;
+		
+		int r1 = true ? x : y;
+		System.out.println(r1);
+		
+		int r2 = false ? x : y;
+		System.out.println(r2);
+		
+	}
+}

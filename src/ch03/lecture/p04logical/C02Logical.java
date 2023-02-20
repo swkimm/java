12250fa3040c4fbf4156d07e2944d9f2962a57ec
swkimm <swkim7161@kakao.com>
2023-02-20 09:11:53 +0900

    Create C02Logical.java

---
+++ src/ch03/lecture/p04logical/C02Logical.java
@@ -0,0 +1,30 @@
+package ch03.lecture.p04logical;
+
+public class C02Logical {
+
+	public static void main(String[] args) {
+		// ^ (xor : exclusive of (배타적 논리합))
+		// 피연산자가 다르면 결과 true ,같으면 false
+
+		System.out.println(true ^ true); // false
+		System.out.println(true ^ false); // true
+		System.out.println(false ^ true); // true
+		System.out.println(false ^ false); // false
+
+		// & (and)
+		// 피연산자가 모두 true이면 true, 나머지는 false
+		System.out.println(true & true); // true
+		System.out.println(true & false); // false
+		System.out.println(false & true); // false
+		System.out.println(false & false); // false
+
+		// | (and)
+		// 피연산자가 모두 false이면 false, 나머지는 true
+		System.out.println(true | true); // true
+		System.out.println(true | false); // true
+		System.out.println(false | true); // true
+		System.out.println(false | false); // false
+
+	}
+
+}

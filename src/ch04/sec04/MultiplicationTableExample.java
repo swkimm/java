b50675af3aa4b78c0ecabd6a2969619b3096562c
swkimm <swkim7161@kakao.com>
2023-02-21 16:54:22 +0900

    Do it Java 예제

---
+++ src/ch04/sec04/MultiplicationTableExample.java
@@ -0,0 +1,18 @@
+package ch04.sec04;
+
+public class MultiplicationTableExample {
+
+	public static void main(String[] args) {
+		
+		for (int i = 2; i <= 9; i++) {
+			System.out.printf("********%d단 ********%n", i);
+			for (int j = 1; j <= 9; j++) {
+				System.out.printf("%d X %d = %d%n", i,j,i*j);
+			}
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/codetest/codingbat/String2_DoubleChar.java
@@ -0,0 +1,21 @@
+package codetest.codingbat;
+
+public class String2_DoubleChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		String doubleChar(String str) {
+//			int len = str.length();
+//			String ans = "";
+//			for ( int i = 0; i< len; i++ ) {
+//				for ( int j = 0; j < 2; j++) {
+//					ans += str.charAt(i);
+//				}
+//			}
+//			return ans;
+//		}
+//		
+	}
+
+}

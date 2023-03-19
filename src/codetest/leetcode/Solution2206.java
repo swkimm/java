38bb130b691eb8eb6e058708bfd2d63073a4184d
swkimm <swkim7161@kakao.com>
2023-03-19 23:03:44 +0900

    Leetcode 코딩 테스트

--- src/codetest/leetcode/Solution2206.java
+++ src/codetest/leetcode/Solution2206.java
@@ -6,25 +6,34 @@ public class Solution2206 {
 	public boolean divideArray(int[] nums) {
 
 		Map<Integer, Integer> map = new HashMap<>();
+//
+//		// 각 값이 몇개인지 세는 코드
+//		for (int n : nums) {
+//			if (map.containsKey(n)) {
+//				// 키 n의 값을 1더해서 다시 entry에 넣는다
+//				int oldvalue = map.get(n);
+//				map.put(n, oldvalue + 1);
+//
+//			} else {
+//				map.put(n, 1);
+//			}
+//		}
+//		// value가 홀수 인게 있으면 return false;
+//		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//			int v = entry.getValue();
+//			if (v % 2 == 1) {
+//				return false;
+//			}
+//		}
+//		return true;
 
-		// 각 값이 몇개인지 세는 코드
-		for (int n : nums) {
-			if (map.containsKey(n)) {
-				// 키 n의 값을 1더해서 다시 entry에 넣는다
-				int oldvalue = map.get(n);
-				map.put(n, oldvalue + 1);
-
-			} else {
-				map.put(n, 1);
-			}
-		}
-		// value가 홀수 인게 있으면 return false;
-		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-			int v = entry.getValue();
-			if (v % 2 == 1) {
-				return false;
-			}
-		}
-		return true;
+		return map.values()
+				.stream()
+				.allMatch(e -> (e % 2) == 0);
 	}
+
+	private boolean even(int e) {
+		return (e % 2) == 0;
+	}
+
 }

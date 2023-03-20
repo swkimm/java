592a046a2d7ce26e58c893c3f78dd5cdb8f1580e
swkimm <swkim7161@kakao.com>
2023-03-20 16:45:59 +0900

    프로그래머스 코딩테스트

---
+++ src/codetest/programmers/Solution120890.java
@@ -0,0 +1,19 @@
+package codetest.programmers;
+
+import java.util.*;
+
+public class Solution120890 {
+	  public int solution(int[] array, int n) {
+	        int answer = 0;
+	        
+//	        Arrays.sort(array);
+	        
+	        for(int num : array) {
+	        		if(num > n) {
+	        			
+	        		}
+	        }
+	        
+	        return answer;
+	    }
+}
